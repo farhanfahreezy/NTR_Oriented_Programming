87035fc55705a21541d81bb579c773cb376a953a
arieljovananda88 <jovanandaa@gmail.com>
2023-03-04 17:37:12 +0700

    Added ability deck const

--- src/AbilityDeck.cpp
+++ src/AbilityDeck.cpp
@@ -2,38 +2,17 @@
 
 using namespace std;
 
-AbilityDeck :: AbilityDeck() : Deck(0){
+AbilityDeck :: AbilityDeck() : Deck<AbilityCard>(0){
     this->amount = 0;
 }
 
-AbilityDeck :: AbilityDeck(int amount) : Deck(amount){
-    /*for (int i = 0; i < amount; i++) {
-        std::string ability;
-        switch (i % 7) {
-            case 0:
-                ability = "Re-Roll";
-                break;
-            case 1:
-                ability = "Quadruple";
-                break;
-            case 2:
-                ability = "Quarter";
-                break;
-            case 3:
-                ability = "Reverse Direction";
-                break;
-            case 4:
-                ability = "Swap Card";
-                break;
-            case 5:
-                ability = "Switch";
-                break;
-            case 6:
-                ability = "Abilityless";
-                break;
-        }
-        this->cards.push_back(ability);
-    }*/
+AbilityDeck :: AbilityDeck(int amount) : Deck<AbilityCard>(amount){
+    float id = 1;
+    for (int i = 0; i < amount; i++) {
+        AbilityCard abCard(id);
+        this->cards.push_back(abCard);
+        id++;
+    }
 }
 
 /*int main(){
--- src/RegularDeck.cpp
+++ src/RegularDeck.cpp
@@ -9,7 +9,7 @@ RegularDeck :: RegularDeck() : Deck(0){
 RegularDeck :: RegularDeck(int amount) : Deck(amount){
     this->amount = amount;
     for(int i =0; i < amount; i++){
-        RegularCard regCard(i, i%4);
+        RegularCard regCard(i+1, i%4);
         this->cards.push_back(regCard);
     }
 }
