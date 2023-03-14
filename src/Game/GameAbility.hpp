479f0da834199654401b1b94d2077af1d4cc2c6a
shidqizh <shidqizh@gmail.com>
2023-03-15 00:49:14 +0700

    ability revisi spek

--- src/Game/GameAbility.hpp
+++ src/Game/GameAbility.hpp
@@ -1,17 +1,36 @@
 #ifndef GAMEABILITY_HPP
 #define GAMEABILITY_HPP
 #include <Ability/Ability.hpp>
+#include <Game/GameState.hpp>
+#include <InventoryHolder/InventoryHolder.hpp>
 using namespace std;
 
-class GameAbility{
+class AbilityReroll: public Ability{
     public:
-        static const Ability
-            ABILITY_REROLL,
-            ABILITY_QUADRUPLE,
-            ABILITY_QUARTER,
-            ABILITY_REVERSE,
-            ABILITY_SWAP,
-            ABILITY_SWITCH,
-            ABILITY_ABILITYLESS;
+        void get() const override;
+};
+class AbilityQuadruple: public Ability{
+    public:
+        void get() const override;
+};
+class AbilityQuarter: public Ability{
+    public:
+        void get() const override;
+};
+class AbilityReverse: public Ability{
+    public:
+        void get() const override;
+};
+class AbilitySwap: public Ability{
+    public:
+        void get() const override;
+};
+class AbilitySwitch: public Ability{
+    public:
+        void get() const override;
+};
+class AbilityAbilityless: public Ability{
+    public:
+        void get() const override;
 };
 #endif
