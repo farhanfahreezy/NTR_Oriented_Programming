5586f46ec46ab5431c677ad33e845746291e090a
shidqizh <shidqizh@gmail.com>
2023-03-15 01:13:19 +0700

    add dikit

--- src/Game/GameAbility.cpp
+++ src/Game/GameAbility.cpp
@@ -18,11 +18,16 @@ void AbilityQuarter::get() const {
     int tmp = table.getPoint();
 
     if(tmp > 2){
-        table.increasePointByScale(0.25);
-        cout << "Pemain " << name << " melakukan QUARTER! Poin hadiah turun dari " << tmp << " menjadi " << table.getPoint() << "!" << endl;
+        if(table.getPoint() < 1 << 30){
+            table.increasePointByScale(0.25);
+            cout << "Pemain " << name << " melakukan QUARTER! Poin hadiah turun dari " << tmp << " menjadi " << table.getPoint() << "!" << endl;
+        }else{
+            cout << "Poin hadiah (" << table.getPoint() << ") sudah maksimal!" << endl;
+            cout << "Melewati giliran pemain " << name << endl;
+        }
     }else{
-        cout << "Poin hadiah (" << table.getPoint() << ") sudah maksimal!" << endl;
-        cout << "Melewati giliran pemain " << name << endl;
+        table.setPoint(1);
+        cout << "Pemain " << name << " melakukan QUARTER! Poin hadiah turun dari " << tmp << " menjadi 1!" << endl;
     }
 }
 
