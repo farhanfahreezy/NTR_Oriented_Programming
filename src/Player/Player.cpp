b0754cf353aa2e9fb6d3d05006c7472cffd8ca8d
JerichoFletcher <jericho.russel17@gmail.com>
2023-03-16 01:49:22 +0700

    add: static const IDs, ability commands, player abilityless flag

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -11,12 +11,14 @@ Player::Player(int id): /*regularDeck(2), abilityDeck(2)*/ InventoryHolder(0, 0,
     this->id = id;
     this->nama = "";
     this->poin = 0;
+    this->abilityMati = false;
 }
 
 Player::Player(): /*regularDeck(2), abilityDeck(2)*/ InventoryHolder(0, 0, 0){
     this->id = -1;
     this->nama = "";
     this->poin = 0;
+    this->abilityMati = false;
 }
 
 Player& Player::operator=(const Player& other){
@@ -47,6 +49,10 @@ void Player::addPoin(int newPoin){
     poin += newPoin;
 }
 
+bool Player::hasAbilityCard(float id) const{
+    return abilityInv.size() > 0 && abilityInv.at(0).value() == id;
+}
+
 string Player::getName() const{
     return this->nama;
 }
@@ -55,10 +61,18 @@ int Player::getId() const{
     return this->id;
 }
 
+bool Player::getAbilityMati() const{
+    return this->abilityMati;
+}
+
+void Player::setAbilityMati(bool b){
+    this->abilityMati = b;
+}
+
 void Player::toFile(File::Write& writer) const{
     InventoryHolder::toFile(writer);
     writer << nama << '\n';
-    writer << id << ' ' << poin << '\n';
+    writer << id << ' ' << poin << ' ' << abilityMati << '\n';
 }
 
 void Player::fromFile(File::Read& reader){
@@ -72,4 +86,6 @@ void Player::fromFile(File::Read& reader){
     id = stoi(s);
     getline(ss, s, ' ');
     poin = stoi(s);
+    getline(ss, s, ' ');
+    abilityMati = stoi(s);
 }
